7cf8f2e66bc5314b06c3661c62899536cefc9332
东仔 <20193751@stu.neu.edu.cn>
2021-08-21 14:37:52 +0800

    随机数组赋值

--- project.cc
+++ project.cc
@@ -1,10 +1,21 @@
 #include<iostream>
-
+#include<vector>
+#include<iterator>
 using namespace std;
 
 int main()
 {
 
-	cout<<"hello dongzai666"<<endl;
+	int a[10];
+	srand(time(NULL));
+	auto  *p1 =begin(a),*p2 =end(a);
+	while(p1!=p2)
+	{
+
+		*p1++=rand()%50;
+	}
+	cout<<"数组的值为："<<endl;
+	for(auto i:a)
+		cout<<i<<" ";
 	return 0;
 }
